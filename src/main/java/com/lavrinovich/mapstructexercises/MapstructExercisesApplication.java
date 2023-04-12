4cb917b8694e52707a018f458d283f0caf1ca497
Alexey Lavrinovich <lavrinleha95@gmail.com>
2023-04-12 03:51:42 -0700

    Init commit

---
+++ src/main/java/com/lavrinovich/mapstructexercises/MapstructExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.lavrinovich.mapstructexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavrinovich/mapstructexercises/MapstructExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavrinovich.mapstructexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
